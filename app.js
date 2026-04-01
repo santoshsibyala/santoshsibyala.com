42d869acf8d9280df88e0f996a8a1aeb44b74ddf
santoshsibyala <santoshsibyala@gmail.com>
2026-04-01 12:08:12 -0700

    Added websocket to the server

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 const http = require('http');
 
 http.createServer(function(req,res){
-
 	res.write("Hello World! Something big is coming.....");
 	res.end();
-
-
-
-
-}
-	
-
-).listen(3000);
+}).listen(3000);
 
 
 console.log("Server Started on port 3000");
